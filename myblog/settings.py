4fa415ae30fa68929eb46e7f6da9eef904205d16
Pipeloluwa Adebayo <pipeloluwapraise@gmail.com>
2021-10-29 13:50:48 +0100

    initial commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,17 +124,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-MEDIA_URL = '/media/'
+ 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA/')
 
 LOGIN_REDIRECT_URL = ('index')
 LOGOUT_REDIRECT_URL = ('login')
 
 APPEND_SLASH = True
+django_heroku.settings(locals())
